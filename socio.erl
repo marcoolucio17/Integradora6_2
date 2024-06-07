fb9e1094d2ef8687e082be7e1fc5a54598c77e6f
gmujicap <A01285409@tec.mx>
2024-06-06 22:25:38 -0600

    Pedidos Fix and General Documentation

--- socio.erl
+++ socio.erl
@@ -1,24 +1,35 @@
+% Actividad Integradora 6.2
+% Modulo Socio
+
+% Marco Antonio Lucio Sosa
+% Gabriel Ernesto Mujica Proulx
+% Germán Salas
+% Alejandro Charles
+
 -module(socio).
 -export([suscribir_socio/1, elimina_socio/1, crea_pedido/2, lista_existencias/0]).
 
+% Se agrega un nuevo socio a la lista
 suscribir_socio(Socio) ->
     Caller = self(),
     io:format("Manda: ~p solicita suscripción~n", [Socio]),
-    tienda ! {suscribir_socio, Socio, Caller},
+    tienda ! {suscribir_socio, Socio, Caller}, % Se comunica con el proceso tienda
     receive
         {ok, Socio} -> {ok, Socio};
         {error, Reason} -> {error, Reason}
     end.
 
+% Se elimina un socio seleccionado de la lista de socios
 elimina_socio(Socio) ->
     Caller = self(),
     io:format("Manda: ~p solicita eliminación de suscripción~n", [Socio]),
-    tienda ! {elimina_socio, Socio, Caller},
+    tienda ! {elimina_socio, Socio, Caller}, % Se comunica con el proceso tienda para realizar el cambio
     receive
         {ok, Socio} -> {ok, Socio};
         {error, Reason} -> {error, Reason}
     end.
 
+% Se crea un pedido de productos, con una lista de tuplas que incluyen producto y cantidad
 crea_pedido(Socio, ListaDeProductos) ->
     Caller = self(),
     io:format("Manda: ~p crea pedido con productos ~p~n", [Socio, ListaDeProductos]),
@@ -28,6 +39,7 @@ crea_pedido(Socio, ListaDeProductos) ->
         {error, Reason} -> {error, Reason}
     end. 
 
+% Se manda a imprimir una lista de los productos disponibles y sus cantidades
 lista_existencias() ->
     Caller = self(),
     io:format("Manda: solicita lista de existencias~n"),
--- tienda.erl
+++ tienda.erl
@@ -1,4 +1,7 @@
-% Marco Antonio Lucio Sosa A01285589
+% Actividad Integradora 6.2
+% Modulo Tienda
+
+% Marco Antonio Lucio Sosa
 % Gabriel Ernesto Mujica Proulx
 % Germán Salas
 % Alejandro Charles
@@ -6,22 +9,27 @@
 -module(tienda).
 -export([abre_tienda/0, cierra_tienda/0, registra_producto/2, elimina_producto/1, modifica_producto/2, listar_existencias/0, lista_socios/0, productos_vendidos/0]).
 
+% Inicializamos el proceso de la tienda
 abre_tienda() -> 
     register(tienda, spawn(fun() -> tienda_loop([], [], [], 0) end)),
     io:format("Tienda iniciada~n").
 
+% Se manda mensaje para terminar la tienda
 cierra_tienda() ->
     tienda ! exit.
 
+    
+% Registramos el producto
 registra_producto(Producto, Cantidad) ->
     Caller = self(),
-    io:format("Manda: ~p solicita registrar producto con cantidad ~p~n", [Producto, Cantidad]),
+    io:format("Manda: Se solicita registrar producto ~p con cantidad ~p~n", [Producto, Cantidad]),
     tienda ! {registra_producto, Producto, Cantidad, Caller},
     receive
         {ok, _} -> ok;
         {error, Reason} -> {error, Reason}
     end.
 
+% Se elimina el producto ingresado
 elimina_producto(Producto) ->
     Caller = self(),
     io:format("Manda: ~p solicita eliminar producto~n", [Producto]),
@@ -31,15 +39,17 @@ elimina_producto(Producto) ->
         {error, Reason} -> {error, Reason}
     end.
 
+% Modificamos cierto producto en su cantidad
 modifica_producto(Producto, Cantidad) ->
     Caller = self(),
-    io:format("Manda: ~p solicita modificar producto con cantidad ~p~n", [Producto, Cantidad]),
+    io:format("Manda: ~p solicita modificar producto por cantidad ~p~n", [Producto, Cantidad]),
     tienda ! {modifica_producto, Producto, Cantidad, Caller},
     receive
         {ok, _} -> ok;
         {error, Reason} -> {error, Reason}
     end.
 
+% Mandamos a imprimir la lista de todos los productos en existencia
 listar_existencias() ->
     Caller = self(),
     io:format("Solicita lista de existencias~n"),
@@ -49,6 +59,7 @@ listar_existencias() ->
         {error, Reason} -> {error, Reason}
     end.
 
+% Se manda a imprimir una lista de todos los socios suscritos
 lista_socios() ->
     Caller = self(),
     io:format("Solicita lista de socios~n"),
@@ -58,6 +69,7 @@ lista_socios() ->
         {error, Reason} -> {error, Reason}
     end.
 
+% Se manda a imprimir una lista con todos los pedidos realizados
 productos_vendidos() ->
     Caller = self(),
     io:format("Solicita lista de productos vendidos~n"),
@@ -67,58 +79,74 @@ productos_vendidos() ->
         {error, Reason} -> {error, Reason}
     end.
 
+% Loop del proceso tienda
 tienda_loop(Socios, Productos, Pedidos, ContadorPedidos) ->
     receive
+        % Se detiene el proceso
         exit ->
             io:format("Tienda cerrada~n"),
-            terminate_productos(Productos),
+            terminate_productos(Productos), % Se detienen todos los procesos de productos
             unregister(tienda),
             exit(normal);
+        % Creamos el proceso de producto y registramos un producto con cierta cantidad
         {registra_producto, Producto, Cantidad, Caller} -> 
             io:format("Recibe: solicitud de registrar producto ~p con cantidad ~p~n", [Producto, Cantidad]),
-            Pid = spawn(fun() -> producto_loop(Producto, Cantidad) end),
+            Pid = spawn(fun() -> producto_loop(Producto, Cantidad) end), % Creamos el proceso y guardamos el pid
             Caller ! {ok, Producto},
-            tienda_loop(Socios, [{Producto, Pid} | Productos], Pedidos, ContadorPedidos);
+            tienda_loop(Socios, [{Producto, Pid} | Productos], Pedidos, ContadorPedidos); % El producto y su pid se guarda en la lista
+        % Se elimina el producto seleccionado de existencias
         {elimina_producto, Producto, Caller} ->
             io:format("Recibe: solicitud de eliminar producto ~p~n", [Producto]),
+            % Se busca el producto en el inventario, validando que existe
             case lists:keyfind(Producto, 1, Productos) of
                 false ->
+                    % En caso de NO encontrarlo, se le informa al proceso y reinicia
                     Caller ! {error, not_found},
                     tienda_loop(Socios, Productos, Pedidos, ContadorPedidos);
                 {Producto, Pid} ->
+                    % En caso de encontrarlo, se elimina
                     exit(Pid, kill),
                     Caller ! {ok, Producto},
                     tienda_loop(Socios, lists:keydelete(Producto, 1, Productos), Pedidos, ContadorPedidos)
             end;
+        % En base a cierta cantidad, modificamos un mensaje a cierto Pid especifico de modificar su cantidad
         {modifica_producto, Producto, Cantidad, Caller} ->
             io:format("Recibe: solicitud de modificar producto ~p con cantidad ~p~n", [Producto, Cantidad]),
             case lists:keyfind(Producto, 1, Productos) of
+                % Si el producto NO existe, mandamos dicho mensaje y reiniciamos el loop
                 false ->
                     Caller ! {error, not_found},
                     tienda_loop(Socios, Productos, Pedidos, ContadorPedidos);
+                % De existir, mandamos a modificar dicha cantidad
                 {Producto, Pid} ->
                     Pid ! {modificar, Cantidad},
                     Caller ! {ok, Producto},
                     tienda_loop(Socios, Productos, Pedidos, ContadorPedidos)
             end;
+        % Se imprimen los productos existentes
         {listar_existencias, Caller} ->
             io:format("Recibe: solicitud de lista de existencias~n"),
             Existencias = [{Producto, get_cantidad(Pid)} || {Producto, Pid} <- Productos],
             Caller ! {ok, Existencias},
             tienda_loop(Socios, Productos, Pedidos, ContadorPedidos);
+        % Se agrega un socio por su nombre a la lista de socios
         {suscribir_socio, Socio, Caller} ->
             io:format("Recibe: solicitud de suscripción de ~p~n", [Socio]),
             case lists:member(Socio, Socios) of
+                % Si el socio ya existe, marca error
                 true ->
                     Caller ! {error, already_exists},
                     tienda_loop(Socios, Productos, Pedidos, ContadorPedidos);
+                % Si no, se agrega a la lista
                 false ->
                     Caller ! {ok, Socio},
                     tienda_loop([Socio | Socios], Productos, Pedidos, ContadorPedidos)
             end;
+        % Se elimina un socio seleccionado de la lista de socios
         {elimina_socio, Socio, Caller} ->
             io:format("Recibe: solicitud de eliminación de suscripción de ~p~n", [Socio]),
             case lists:member(Socio, Socios) of
+                % Se valida la existencia del socio en la lista
                 true ->
                     Caller ! {ok, Socio},
                     tienda_loop(lists:delete(Socio, Socios), Productos, Pedidos, ContadorPedidos);
@@ -126,46 +154,67 @@ tienda_loop(Socios, Productos, Pedidos, ContadorPedidos) ->
                     Caller ! {error, not_found},
                     tienda_loop(Socios, Productos, Pedidos, ContadorPedidos)
             end;
+        % Se crea un pedido de un socio, con una lista de tuplas
         {crea_pedido, Socio, ListaDeProductos, Caller} ->
             io:format("Recibe: solicitud de creación de pedido de ~p con productos ~p~n", [Socio, ListaDeProductos]),
             case lists:member(Socio, Socios) of
+                % Si el socio no existe, no se realiza el pedido
                 false ->
                     Caller ! {error, not_a_member},
                     tienda_loop(Socios, Productos, Pedidos, ContadorPedidos);
                 true ->
                     NuevoContadorPedidos = ContadorPedidos + 1,
+                    % Se crea una lista con el nombre del producto, el pid de su proceso, y la cantidad a modificar
+                    Venta = [{Pid, Cantidad} || {Producto, Pid} <- Productos, {ok, Cantidad} <- [{ok, C} || {P, C} <- ListaDeProductos, P =:= Producto]],                    modificar_productos(Venta), % Funcion que modifica productos de un pedido
                     Pedido = {NuevoContadorPedidos, Socio, ListaDeProductos},
                     Caller ! {ok, Pedido},
+                    % Se reinicia el loop con la nueva informacion
                     tienda_loop(Socios, Productos, [Pedido | Pedidos], NuevoContadorPedidos)
             end;
+        % Se imprime una lista de los socios en existencia
         {lista_socios, Caller} ->
             io:format("Recibe: solicitud de lista de socios~n"),
             Caller ! {ok, Socios},
             tienda_loop(Socios, Productos, Pedidos, ContadorPedidos);
+        % Se imprime una lista con todos los pedidos realizados
         {productos_vendidos, Caller} ->
             io:format("Recibe: solicitud de productos vendidos~n"),
             Caller ! {ok, Pedidos},
             tienda_loop(Socios, Productos, Pedidos, ContadorPedidos)
     end.
 
-terminate_productos([]) -> ok;
-terminate_productos([{_, Pid} | T]) ->
+% Terminar procesos de productos
+terminate_productos([]) -> ok; % Si no hay nada, regresamos ok
+terminate_productos([{_, Pid} | T]) -> 
+    % Si existen procesos de productos aún, los eliminamos
     exit(Pid, kill),
     terminate_productos(T).
 
+% Se modifican las cantidades de cada producto en un pedido recursivamente
+modificar_productos([]) -> ok; % Si la lista no tiene elementos, regresa ok
+modificar_productos([{Pid, Cant} | T]) ->
+    Pid ! {modificar, -Cant}, % Se manda al proceso del producto a modificar
+    modificar_productos(T).
+
+% Bucle del proceso del producto
+% Se llama para crear el proceso, y recibe mensajes para modificar o conseguir informacion
 producto_loop(Producto, Cantidad) ->
     receive
+        % Se modifica la cantidad del producto
         {modificar, NuevaCantidad} ->
-            io:format("Producto ~p modificado a ~p~n", [Producto, NuevaCantidad]),
+            io:format("Producto ~p modificado por ~p~n", [Producto, NuevaCantidad]),
             producto_loop(Producto, max(0, Cantidad + NuevaCantidad));
+        % Manda la cantidad del producto a la funcion caller
         {get_cantidad, Caller} ->
             Caller ! {cantidad, Cantidad},
             producto_loop(Producto, Cantidad);
+        % Se termina el proceso
         stop ->
             io:format("Producto ~p detenido~n", [Producto]),
             exit(normal)
     end.
 
+%Se obtiene la cantidad del producto
 get_cantidad(Pid) ->
     Pid ! {get_cantidad, self()},
     receive
